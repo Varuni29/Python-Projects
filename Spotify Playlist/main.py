122626ba106ef42dcef4f9d60dec19d9bcdf420f
Varuni29 <129275484+Varuni29@users.noreply.github.com>
2023-04-15 11:50:13 +0530

    Add files via upload

--- Spotify Playlist/main.py
+++ Spotify Playlist/main.py
@@ -2,9 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import os
 
-CLIENT_ID="077b53805a0c441c8310bc260b960d79"
-CLIENT_SECRET="dd5e872d053a42fe8b61813e8d34a5b1"
+CLIENT_ID=os.environ["client_id"]
+CLIENT_SECRET=os.environ["client_secret"]
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
                          scope="playlist-modify-private",
@@ -15,8 +16,6 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
                          cache_path="token.txt"))
 
 user_id=sp.current_user()["id"]
-
-# songs_names=["Flowers","Hey Soul Sister","Sunflower"]
 
 date=input("What year you would like to travel to? Type in YYY-MM-DD format: ")
 
@@ -43,5 +42,4 @@ for song in songs_names:
         print(f"{song} doesn't exist in Spotify. Skipped.")
 
 playlist=sp.user_playlist_create(user=user_id,name=f"{year} Billboard 100",public=False)
-# print(playlist)
 sp.user_playlist_add_tracks(playlist_id=playlist["id"],tracks=song_uris,user=user_id)
